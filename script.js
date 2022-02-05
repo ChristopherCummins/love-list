a5fcf0f2d86ec9ba7cc640fddfef0b0d20ff3c18
Christopher Cummins <christopher.francis.cummins@gmail.com>
2022-02-05 13:38:36 -0500

    project started

---
+++ script.js
@@ -0,0 +1,27 @@
+function getRandomNumber() {
+    let possibleOutcomes = loveList.length;
+    console.log(Math.floor(Math.random() * possibleOutcomes));
+}
+
+let loveList = [
+    "I love that you answer the phone when your sponsees call you",
+    "I love the way you pronounce the second syllable in the word 'hilarious'",
+    "I love that living with you feels like every day is Christmas (raining packages)",
+    "I love the kind of attentive and caring father you are to Belle",
+    "I love how you squeeze my feet when they're tense",
+    "I love the way you reply 'a book' when I ask you what you bought",
+    "I love the way you guide out of danger's way",
+    "I love your willingness to explain astrology to willing ears",
+    "I love the honesty you display with yourself",
+    "I love that you choose to cheer me on with nearly everything I do",
+    "I love that you seek to teach rather than judge",
+    "I love that you keep your spiritual center open for growth and learning opportunities",
+    "I love the way you make it a point to help other people",
+    "I love the generosity you give your friends",
+    "I love the way you know when to gently set a boundary to care for yourself",
+    "I love the way you say 'WHAAAT?' when you realize you have an chance to introduce me to something new",
+    "I love that you share your love with my family",
+    "I love that you are willing to share your recovery and insights publicly on social media",
+    "I love that you call so many things 'toys'",
+    "I love when you scream laugh"
+    ];
